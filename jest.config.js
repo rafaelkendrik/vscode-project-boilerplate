d6664aef1bb7ddafe868086dc45e64303ea4de6e
Rafael Kendrik <rafaelkendrik@gmail.com>
2020-12-06 18:23:28 -0300

    chore: add prettier and a few improvements to existent config files

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  testEnvironment: "node",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
